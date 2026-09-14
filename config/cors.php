0dda7e7b59e7dfebaf89de06d0ff86244415b1c4
Rennan Alves <rennandev7@gmail.com>
2026-09-14 13:50:09 -0400

    fix: corrigindo problema de cors

--- config/cors.php
+++ config/cors.php
@@ -12,6 +12,7 @@ return [
         'http://localhost:5174',
         'http://127.0.0.1:5174',
         env('FRONTEND_URL', ''),
+        env('ANALYTICS_URL', ''),
     ])),
 
     'allowed_origins_patterns' => [],
